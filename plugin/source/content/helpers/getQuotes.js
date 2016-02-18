ef97e55190cd2b09c2fec7cfdc520742c5eefafb
Yoshio Varney <yoshio.varney@gmail.com>
2016-02-17 22:18:05 -0800

    Started styling ticker

--- plugin/source/content/helpers/getQuotes.js
+++ plugin/source/content/helpers/getQuotes.js
@@ -4,20 +4,27 @@ export default (symbols) => {
   let html = '';
   const last = symbols.length - 1;
 
-  getQuote(symbols).then((data) => {
-    data.forEach((stock, index) => {
-      html += createSpan(stock, index, last);
-    })
-    appendHTML(html);
-  });
+  if (symbols.length > 0) {
+    getQuote(symbols).then((data) => {
+      data.forEach((stock, index) => {
+        html += createSpan(stock, index, last);
+      })
+      appendHTML(html);
+    });
+  }
 }
 
 function createSpan(stock, index, last) {
   let symbol = stock.resource.fields;
-  let sym = symbol.symbol;
+  console.log(symbol)
+  // let sym = symbol.symbol;
+  let sym = `<a class="stockLink" target="_blank" href="http://finance.yahoo.com/q?s=${symbol.symbol}">${symbol.symbol}</a>`;
   let price = Number(symbol.price).toFixed(2);
   let chg = Number(symbol.chg_percent).toFixed(2);
-  let span = `<span> ${sym} ${price} ${chg}%`;
+  let span = `<span class="stockFont"> ${sym} ${price} <span class="`;
+  span += chg[1] !== '-' ? 'positive' : 'negative';
+  // console.log('per', symbol.chg_percent)
+  span += `">${chg}</span>%`;
   if (index !== last) {
     span += ' |';
   }
@@ -27,13 +34,13 @@ function createSpan(stock, index, last) {
 
 function appendHTML(html) {
   const body = document.getElementsByTagName('body')[0];
-  let del = document.getElementById('deleteStock');
+  let del = document.getElementById('stockSpan');
   let element = document.createElement('span');
 
   if (del) {
     del.remove();
   }
-  element.setAttribute('id', 'deleteStock');
+  element.setAttribute('id', 'stockSpan');
   element.innerHTML = html;
   body.insertBefore(element, body.firstChild);
 }
